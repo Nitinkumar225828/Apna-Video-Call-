a44760e198ea62301629cced8f9b9a4070a846a9
Nitin Kumar <Nitin.kumar225828@gmail.com>
2025-09-26 00:23:38 +0530

    commit in app.js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from "./pages/LandingPage.jsx"
 import Authentication from './pages/Authentication.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
-import VideoMeet from './pages/videoMeet.jsx'
+import VideoMeet from './pages/VideoMeet.jsx'
 import HomeComponent from './pages/home.jsx'
 import { History } from './pages/history.jsx'
 
